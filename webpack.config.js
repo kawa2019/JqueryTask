0c598909576814b7f759d0c822307edebdc06cd2
Kamil Kuciak <kamil97kuc@gmail.com>
2020-10-08 18:00:31 +0200

    adding config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist/js")
+    },
+    watch: false,
+    mode: "development",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    }
+}
